31670307bad72b1a519e31429de04156fc05bf95
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2019-08-20 17:05:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log(CryptoJS.AES);
     function load(contentJsonString) {
         content = JSON.parse(contentJsonString).content;
 
-        const p = prompt("p");
+        const p = prompt();
         const value = CryptoJS.AES.decrypt(content, p).toString(CryptoJS.enc.Utf8);
 
         document.body.innerHTML = value;
